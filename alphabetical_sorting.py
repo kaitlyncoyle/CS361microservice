bc1e99a2693a952bace95b442c7d4ff7aed7e359
Kaitlyn Coyle <kaitlyncoyle@Kaitlyns-MacBook-Pro.local>
2022-02-28 00:27:09 -0700

    This commit includes creating the new csv file and writes the sorted list. You can either have the header, or not, hence why it is commented.

--- alphabetical_sorting.py
+++ alphabetical_sorting.py
@@ -1,8 +1,14 @@
 import csv
-from operator import itemgetter
 
-with open('data.csv', 'r') as f:
+# header = ['TAG', 'DATA']
+with open("data.csv", "r") as f:
     data = [line for line in csv.reader(f)]
 
 data.sort()
-print(data)
+
+with open("new_data.csv", "w") as f:
+    writer = csv.writer(f)
+    # writer.writerow(header)
+    writer.writerow(data)
+
+# print(data)
