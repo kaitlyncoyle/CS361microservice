6cec4ea2dff0894b15d27a31e10ff42bc9fe96eb
Kaitlyn Coyle <kaitlyncoyle@Kaitlyns-MacBook-Pro.local>
2022-02-27 17:35:05 -0700

    This is my inital commit, and it is also the commit that has the complete code for the microservice. If it doesn't work, let me know and I will edit it.

---
+++ alphabetical_sorting.py
@@ -0,0 +1,7 @@
+# you will have your program communicate all of the data
+# into the list that will then sort it in the correct alphabetical order
+macro_value_list = []
+macro_value_list.sort()
+# after the data is sorted, you will have it communicate
+# back to your program and have it show the data sorted
+# on the UI.
